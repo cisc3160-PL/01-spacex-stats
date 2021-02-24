6a93c0b6440ef9ec34e8def374deddcfbef63a28
Ricky Chon <rickychon99@gmail.com>
2021-02-24 07:18:25 -0500

    Replace placeholders with Starship data

--- scripts/spacex-data.js
+++ scripts/spacex-data.js
@@ -10,4 +10,40 @@ fetch('https://api.spacexdata.com/v4/rockets',
 })
 .then(response => response.json())
 .then(data => rockets = data)
-.then(() => console.log(rockets));
+.then(() => 
+{
+    console.log(rockets[3]);
+    let rocket = rockets[3];
+    updateRocketName(rocket.name);
+    updateRocketDesc(rocket.description);
+    updateRocketAttribute(rocket.height, 'height', 'meters', 'feet');
+    updateRocketAttribute(rocket.diameter, 'diameter', 'meters', 'feet');
+    updateRocketAttribute(rocket.mass, 'mass', 'kg', 'lb');
+    updateRocketAttribute(rocket.payload_weights[0], 'payload', 'kg', 'lb');
+    updateImage(1, rocket.flickr_images[2]);
+    updateImage(2, rocket.flickr_images[3]);
+});
+
+function updateRocketName(name)
+{
+    document.getElementById('rocket-name').textContent = name.toUpperCase();
+}
+
+function updateRocketDesc(desc)
+{
+    document.getElementById('rocket-desc').textContent = desc;
+}
+
+function updateRocketAttribute(attribute, name, metric, imperial)
+{
+    let metricEl = document.getElementById(`${name}-metric`).textContent;
+    let imperialEl = document.getElementById(`${name}-imperial`).textContent;
+    document.getElementById(`${name}-metric`).textContent = metricEl.replace(/\?+/, attribute[metric]);
+    document.getElementById(`${name}-imperial`).textContent = imperialEl.replace(/\?+/, attribute[imperial]);
+}
+
+function updateImage(num, src)
+{
+    let img = document.getElementById(`rocket-img${num}`);
+    img.src = src;
+}
